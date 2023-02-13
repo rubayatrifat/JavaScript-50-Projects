515400273db6910a461fce1571aa70ae523caedd
rubayatriat <rubayatislam17@gmail.com>
2023-02-13 23:43:53 +0600

    Event KeyCode

--- Day - 11 - Event KeyCode(12-Feb-23)/main.js
+++ Day - 11 - Event KeyCode(12-Feb-23)/main.js
@@ -1,1 +1,20 @@
-        
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (evt) => {
+    insert.innerHTML = ` 
+    <div class="key">
+        ${evt.key === ' ' ? 'Space' : evt.key}
+        <small>evt.key</small>
+    </div>
+
+    <div class="key">
+        ${evt.keyCode}
+        <small>event.keyCode</small>
+    </div>
+
+    <div class="key">
+        ${evt.code}
+        <small>event.code</small>
+    </div>
+`
+})
