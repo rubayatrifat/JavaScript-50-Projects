26a32f9242648d6858dba59bb3300e19090a3358
rubayatriat <rubayatislam17@gmail.com>
2023-03-02 18:32:24 +0600

    Them Clock

--- Day-19-Them Clock (27-Feb-23)/main.js
+++ Day-19-Them Clock (27-Feb-23)/main.js
@@ -2,4 +2,22 @@ const hourELm = document.querySelector('.hour')
 const minuteElm = document.querySelector('.minute')
 const scoundElm = document.querySelector('.scound')
 const timeElm = document.querySelector('.time')
-const dateElm = document.querySelector('.hour')
+const dateElm = document.querySelector('.date')
+const toogle = document.querySelector('.toggle')
+
+
+const days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednessday", "Thursday", "Friday"]
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Sep", "Oct", "Nov", "Dec"]
+
+toogle.addEventListener('click', (e) => {
+    const hmtl = document.querySelector('html')
+
+    if(hmtl.classList.contains('dark')) {
+        hmtl.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        hmtl.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+})
