e90b36ea8e06886509b65dece33ac0d75ed8b40c
rubayatrifat <rubayatislam17@gmail.com>
2023-04-02 15:52:14 +0600

    Freeback UI

---
+++ Day-43-Free Back UI Design(-Mar-23)/main.js
@@ -0,0 +1,7 @@
+const ratings = document.querySelectorAll('.rating')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+
+panel.addEventListener('click', () => {
+    console.log('click ')
+})
